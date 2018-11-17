d5672a47d2d332120134566c4cde91f8ca25b145
apu-izz <31754074+apu-izz@users.noreply.github.com>
2018-11-16 22:38:33 -0600

    change

--- CommonInputControls/CommonInputControls/ViewController.swift
+++ CommonInputControls/CommonInputControls/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func CommonControl(_ sender: Any) {
+        print("Button was tapped!")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- Two Buttons/Two Buttons/ViewController.swift
+++ Two Buttons/Two Buttons/ViewController.swift
@@ -15,6 +15,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func FirstButton(_ sender: UISwitch) {
+        if sender.isOn {
+            print("The switch is on!")
+        } else {
+            print("The switch is off.")
+        }
+    }
+
+    @IBAction func SocendButton(_ sender: UISlider) {
+        print(sender.value)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
